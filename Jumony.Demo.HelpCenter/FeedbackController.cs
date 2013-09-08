ee55282e7d31c7342d32ec1db8338e1dbbaba686
Ivony <Ivony@live.com>
2013-09-08 22:32:30 +0800

    增加feedback功能

---
+++ Jumony.Demo.HelpCenter/FeedbackController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jumony.Demo.HelpCenter
+{
+  public class FeedbackController
+  {
+
+    public AddFeedback( string helpPath, string content )
+    {
+    
+    }
+
+  }
+}
