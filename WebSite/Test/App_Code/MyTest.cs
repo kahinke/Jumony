5db0971d02747e15e75a1cd479fda7cf8c86c7c1
Ivony <Ivony@live.com>
2013-10-31 15:28:06 +0800

    完善 WebTest 框架

--- WebSite/Test/App_Code/MyTest.cs
+++ WebSite/Test/App_Code/MyTest.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using WebTest;
+using Ivony.Web;
 
 public class MyTest : TestClass
 {
 
   public void Test1()
-  { 
-    
-    
+  {
 
+    var service = new MyTest();
+
+    WebServices.RegisterService( service, "~/" );
+
+    Assert.AreEqual( service, WebServices.GetServices<MyTest>( "~/abc" ).FirstOrDefault() );
+  }
+
+  public void Test2()
+  {
+    Assert.AreEqual( 2, 2 );
+
+  }
+
+  public void Test3()
+  {
+    throw new Exception( "TMD" );
   }
 
 }
