d69bf507decfc7be19a9cd7d35e780fee0c43ec6
Ivony <Ivony@live.com>
2013-11-01 11:13:30 +0800

    完善 WebTest 框架

---
+++ WebTest/TestResultException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebTest
+{
+  public class TestResultException : TestResult
+  {
+
+
+    public TestResultException( TestInfo info, Exception exception )
+      : base( info )
+    {
+      Exception = exception;
+    }
+
+
+    public Exception Exception
+    {
+      get;
+      private set;
+
+    }
+
+
+    public override string Message
+    {
+      get
+      {
+        return string.Format( "执行测试中发生类型为 {0} 的异常，异常信息为： {1}", Exception.GetType().FullName, Exception.Message );
+      }
+    }
+
+    public override bool IsSuccessed
+    {
+      get
+      {
+        return false;
+      }
+    }
+  }
+}
